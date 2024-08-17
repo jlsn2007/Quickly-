4d9361511e6a95217cf8a414b509a10f3dcc2848
Levi Saravia <20230170@ricaldone.edu.sv>
2024-08-17 10:36:05 -0600

    Initial commit

---
+++ src/Modelo/ClaseConexion.java
@@ -0,0 +1,28 @@
+package Modelo;
+
+import java.sql.*;
+
+public class ClaseConexion {
+    
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String USUARIO = "system";
+    private static final String CONTRASENA = "Oracle2024";
+
+    public static Connection getConexion() {
+        try {
+            // Cargar el driver JDBC
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+ 
+            // Obtener la conexión en una variable
+            Connection conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+            // Retornamos la variable que tiene la conexión
+            return conexion;
+        } catch (SQLException e) {
+            System.out.println("Este es el error" + e);
+              return null;
+        } catch (ClassNotFoundException ex) {
+            System.out.println("este es el error de la clase" + ex);
+              return null;
+        }
+    }
+}
